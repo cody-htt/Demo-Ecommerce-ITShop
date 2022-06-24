5da6a8a097a1ebc8c77d2f307cb9f986bb8065d5
cody-xld <tung@xload.io>
2022-06-25 06:01:24 +0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
 
-const App = () => (
-	<div>
-		<p>My React App</p>
-	</div>
-);
+const App = () => {
+	return (
+		<Switch>
+			<Route exact path='/' component={Home}></Route>
+			<Route exact path='/login' component={Login}></Route>
+			<Route exact path='/register' component={Register}></Route>
+		</Switch>
+	);
+};
 
 export default App;
