4df0ec6c4d6a9f77b9d711595456e53219a3d4a1
cody-xld <tung@xload.io>
2022-06-25 07:09:57 +0700

    Adjust deps version

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
 import {
-	AppstoreOutlined,
+	HomeFilled,
 	SettingOutlined,
 	UserOutlined,
 	UserAddOutlined,
@@ -13,15 +13,19 @@ const Header = () => {
 	const [current, setCurrent] = useState('home');
 
 	const handleClick = (e) => {
-		// console.log(e.key);
 		setCurrent(e.key);
 	};
 
 	return (
 		<Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
-			<Item key='home' icon={<AppstoreOutlined />}>
+			<Item key='home' icon={<HomeFilled />}>
 				Home
 			</Item>
+
+			<SubMenu icon={<SettingOutlined />} title='Username'>
+				<Item key='setting:1'>Option 1</Item>
+				<Item key='setting:2'>Option 2</Item>
+			</SubMenu>
 
 			<Item key='register' icon={<UserAddOutlined />} className='float-right'>
 				Register
@@ -30,11 +34,6 @@ const Header = () => {
 			<Item key='login' icon={<UserOutlined />} className='float-right'>
 				Login
 			</Item>
-
-			<SubMenu icon={<SettingOutlined />} title='Username'>
-				<Item key='setting:1'>Option 1</Item>
-				<Item key='setting:2'>Option 2</Item>
-			</SubMenu>
 		</Menu>
 	);
 };
